b3df8f3caf16d72859d7970c1e94ae63acf3f241
LuizPaulo23 <luizpauloueg@gmail.com>
2023-02-05 04:26:59 -0300

    ADD: Front em Java Finalizado

--- src/ProjectBank/Front.java
+++ src/ProjectBank/Front.java
@@ -1,6 +1,11 @@
 package ProjectBank;
+
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.List;
+import java.util.Scanner;
 
 public class Front extends JFrame {
     private JLabel labelName;
@@ -8,41 +13,71 @@ public class Front extends JFrame {
     private JLabel labelAction;
     private JComboBox<String> comboAction;
     private JLabel labelValue;
-    private JFormattedTextField fieldValue;
+    private JTextField fieldValue;
     private JButton buttonSubmit;
     private JButton buttonCancel;
-    private JButton buttonBalance;
 
     private Bank Transaction = new Bank("0001");
+    private Scanner scanner = new Scanner(System.in);
 
     public Front() {
         setTitle("Transaction");
-        setSize(600, 600);
+        setSize(300, 300);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setLayout(new GridBagLayout());
-        GridBagConstraints constraints = new GridBagConstraints();
+        setLayout(new FlowLayout());
 
         labelName = new JLabel("Name: ");
         fieldName = new JTextField(10);
-        constraints.gridx = 0;
-        constraints.gridy = 0;
-        constraints.insets = new Insets(10, 10, 10, 10);
-        add(labelName, constraints);
-
-        constraints.gridx = 1;
-        constraints.gridy = 0;
-        add(fieldName, constraints);
-
-        labelAction = new JLabel("Action: ");
+        labelAction = new JLabel("Escolha: ");
         comboAction = new JComboBox<>(new String[]{"Deposit", "Withdraw"});
-        constraints.gridx = 0;
-        constraints.gridy = 1;
-        add(labelAction, constraints);
-
-        constraints.gridx = 1;
-        constraints.gridy = 1;
-        add(comboAction, constraints);
-
         labelValue = new JLabel("Value: ");
+        fieldValue = new JTextField(10);
+        buttonSubmit = new JButton("Submit");
+        buttonCancel = new JButton("Cancel");
+
+        add(labelName);
+        add(fieldName);
+        add(labelAction);
+        add(comboAction);
+        add(labelValue);
+        add(fieldValue);
+        add(buttonSubmit);
+        add(buttonCancel);
+
+        buttonSubmit.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                String name = fieldName.getText();
+                Account account = Transaction.generateAccount(name);
+                Transaction.insertAccount(account);
+                String action = comboAction.getSelectedItem().toString();
+                double value = Double.parseDouble(fieldValue.getText());
+                if (action.equals("Deposit")) {
+                    account.deposit(value);
+                } else {
+                    if (!account.withDraw(value)) {
+                        JOptionPane.showMessageDialog(Front.this, "Insufficient balance for withdrawal of " + value);
+                    }
+                }
+                fieldName.setText("");
+                fieldValue.setText("");
+            }
+        });
+
+        buttonCancel.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                List<Account> accountList = Transaction.getAccounts();
+                StringBuilder accounts = new StringBuilder();
+                for (Account cc : accountList) {
+                    accounts.append(cc).append("\n");
+                }
+                JOptionPane.showMessageDialog(Front.this, accounts.toString());
+            }
+        });
+
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        new Front();
     }
 }
