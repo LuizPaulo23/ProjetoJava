00d12020e4d66c4a2d18dfdd254269f17f46dcdc
LuizPaulo23 <luizpauloueg@gmail.com>
2023-02-12 04:10:30 -0300

    CHANGED: Refatorando - add Datas das transações

--- src/ProjectBank/SaveToExcel.java
+++ src/ProjectBank/SaveToExcel.java
@@ -4,11 +4,13 @@ import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
-
 public class SaveToExcel {
     public void writeToExcel(List<Account> accountList, String filePath) {
         Workbook workbook = null;
@@ -22,9 +24,10 @@ public class SaveToExcel {
                 sheet = workbook.createSheet("DBtransaction");
                 Row headerRow = sheet.createRow(0);
                 headerRow.createCell(0).setCellValue("Nome");
-                headerRow.createCell(1).setCellValue("Depósito");
-                headerRow.createCell(2).setCellValue("Saque");
-                headerRow.createCell(3).setCellValue("Saldo");
+                headerRow.createCell(1).setCellValue("Data");
+                headerRow.createCell(2).setCellValue("Depósito");
+                headerRow.createCell(3).setCellValue("Saque");
+                headerRow.createCell(4).setCellValue("Saldo");
                 //headerRow.createCell(4).setCellValue("Senha");
             }
 
@@ -32,11 +35,15 @@ public class SaveToExcel {
             for (Account account : accountList) {
                 Row row = sheet.createRow(rowNum++);
                 row.createCell(0).setCellValue(account.getName());
-                row.createCell(1).setCellValue(account.getDeposito());
-                row.createCell(2).setCellValue(account.getSaque());
-                row.createCell(3).setCellValue(account.getBalance());
-                //row.createCell(4).setCellValue(account.getPassword());
-
+                // Formata a data com o formato especificado
+                SimpleDateFormat formatoData = new SimpleDateFormat("dd-MM-yy");
+                Date dataAtual = new Date();
+                String dataFormatada = formatoData.format(dataAtual);
+                row.createCell(1).setCellValue(dataFormatada);
+                row.createCell(2).setCellValue(account.getDeposito());
+                row.createCell(3).setCellValue(account.getSaque());
+                row.createCell(4).setCellValue(account.getBalance());
+                //row.createCell(5).setCellValue(account.getPassword())
             }
 
             try (FileOutputStream fos = new FileOutputStream(filePath)) {
